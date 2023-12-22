a5f4d1e6fa338571d0e8c723cab8368e4d312734
Igor Baidasov <128513795+IgorBai@users.noreply.github.com>
2023-12-22 13:47:15 +0300

    Created script C.py

---
+++ C.py
@@ -0,0 +1,42 @@
+# Задача C. Тестовый контест Яндекса от 20/12/2023
+
+
+import sys
+
+inp = []
+line = ' '
+while line != '\n':
+    line = sys.stdin.readline()
+    inp.append(line)
+
+for l in range(len(inp)):
+    inp[l] = inp[l].replace('\n', '').strip()
+
+while '' in inp:
+    inp.remove('')
+
+n_kmd = int(inp[0])
+n_kom = int(inp[2])
+
+kmd = [int(x) for x in inp[1].split()]
+kmd.sort()
+
+fff = inp[3:3 + int(inp[2])]
+kom = []
+for u in fff:
+    for k in range(int(u.split()[1])):
+        kom.append(int(u.split()[0]))
+kom.sort()
+
+results = []
+for s in kmd:
+    for r in range(len(kom)):
+        if kom[r] >= s:
+            results.append(1)
+            kom[r] = 0
+            break
+
+if sum(results) == len(kmd):
+    print('Yes')
+else:
+    print('No')
